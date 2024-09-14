a01cbd6978e799e4ecb392dcf232fd4bca324acf
Royne <165766200+RoyneThu0@users.noreply.github.com>
2024-09-14 11:06:04 +0300

    Add files via upload

---
+++ Info Section/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-wallpaper":
+          "url('https://www.ultraimagehub.com/wallpapers/tr:flp-false,gx-0.2,gy-0.5,q-75,rh-3264,rw-5824,th-1080,tw-1920/1242024133290623098.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
