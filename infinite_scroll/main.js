d66f28fec453e611b97e05fed54e35b55af03778
Royne <165766200+RoyneThu0@users.noreply.github.com>
2024-10-06 21:27:39 +0300

    Add files via upload

---
+++ infinite_scroll/main.js
@@ -0,0 +1,19 @@
+export default {
+  theme: {
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
